3547e71524f396416e9dd5638067e5df9b5bdfad
congoimmob-art <congoimmob@gmail.com>
2026-06-13 23:22:03 +0100

    Create app.js

---
+++ config/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("CongoImmo API OK");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
